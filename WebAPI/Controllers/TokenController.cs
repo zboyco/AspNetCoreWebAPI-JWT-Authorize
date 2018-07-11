35b20871f28d2f106c6e4481e0f12522639ec90a
BaiYang <xliu779@foxmail.com>
2018-07-11 11:30:16 +0800

    Create ASP.NET Core WebAPI with JWT

---
+++ WebAPI/Controllers/TokenController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Generate Token
+        /// </summary>
+        /// <param name="id">用户ID</param>
+        /// <param name="role">角色("Client","System","Admin")</param>
+        /// <param name="expiresMinutes">过期时间</param>
+        /// <returns></returns>
+        [HttpGet]
+        public string Get(int id = 1, string role = "Client", int expiresMinutes = 30)
+        {
+            if (true)//用户验证
+            {
+                // push the user’s name into a claim, so we can identify the user later on.
+                var claims = new[]
+                {
+                   new Claim(ClaimTypes.Sid,id.ToString()),
+                   new Claim(ClaimTypes.Role,role),
+               };
+                //sign the token using a secret key.This secret will be shared between your API and anything that needs to check that the token is legit.
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                //.NET Core’s JwtSecurityToken class takes on the heavy lifting and actually creates the token.
+                /**
+                 * Claims (Payload)
+                    Claims 部分包含了一些跟这个 token 有关的重要信息。 JWT 标准规定了一些字段，下面节选一些字段:
+
+                    iss(Issuser)：代表这个JWT的签发主体；
+                    sub(Subject)：代表这个JWT的主体，即它的所有人；
+                    aud(Audience)：代表这个JWT的接收对象；
+                    exp(Expiration time)：是一个时间戳，代表这个JWT的过期时间；
+                    nbf(Not Before)：是一个时间戳，代表这个JWT生效的开始时间，意味着在这个时间之前验证JWT是会失败的；
+                    iat(Issued at)：是一个时间戳，代表这个JWT的签发时间；
+                    jti(JWT ID)：是JWT的唯一标识。
+                    除了规定的字段外，可以包含其他任何 JSON 兼容的字段。
+                 * */
+                var token = new JwtSecurityToken(
+                    issuer: "api." + _configuration["Domain"],
+                    audience: _configuration["Domain"],
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(expiresMinutes),
+                    signingCredentials: creds);
+
+                return "Bearer " + new JwtSecurityTokenHandler().WriteToken(token);
+
+            }
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +13,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace WebAPI
 {
@@ -25,12 +31,69 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //添加jwt验证：
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,//是否验证Issuer
+                        ValidateAudience = true,//是否验证Audience
+                        ValidateLifetime = true,//是否验证失效时间
+                        ValidateIssuerSigningKey = true,//是否验证SecurityKey
+                        ValidAudience = Configuration["Domain"],//Audience
+                        ValidIssuer = "api." + Configuration["Domain"],//Issuer，这两项和前面签发jwt的设置一致
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecurityKey"]))//拿到SecurityKey
+                    };
+                });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss.ffffff";//设置时间格式
+                    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Web API",
+                    Description = "RESTful API for XXX",
+                    Version = "v1",
+                    Contact = new Contact { Name = "xxx", Email = "xxx@xxx.com", Url = "" }
+                });
+                //设置枚举显示为字符串
+                //c.DescribeAllEnumsAsStrings();
+                var apiPath = Path.Combine(Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationBasePath, "WebAPI.xml");
+                c.IncludeXmlComments(apiPath);
+
+                //添加header验证信息
+                //c.OperationFilter<SwaggerHeader>();
+                var security = new Dictionary<string, IEnumerable<string>> { { "Bearer", new string[] { } }, };
+                c.AddSecurityRequirement(security);//添加一个必须的全局安全信息，和AddSecurityDefinition方法指定的方案名称要一致，这里是Bearer。
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT授权(数据将在请求头中进行传输) 参数结构: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",//jwt默认的参数名称
+                    In = "header",//jwt默认存放Authorization信息的位置(请求头中)
+                    Type = "apiKey"
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseAuthentication();//注意添加这一句，启用验证
+
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyHeader();
+                builder.AllowAnyMethod();
+                builder.AllowAnyOrigin();
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,6 +102,15 @@ namespace WebAPI
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MarketingService API v1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
